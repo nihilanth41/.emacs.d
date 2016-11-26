7ece661b54bd4db47663bca8f3fa548465d6dad8
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-26 04:45:18 +0000

    Add hooks for paredit mode

--- init.el
+++ init.el
@@ -1,6 +1,4 @@
-
 (setq gc-cons-threshold 400000000)
-
 ;; Startup
 (when window-system
 (scroll-bar-mode -1)
@@ -36,7 +34,55 @@
   (require 'use-package))
 
 ;; Load config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+;; (org-babel-load-file (concat user-emacs-directory "config.org"))
+(use-package evil
+ :ensure t
+ :config 
+ (evil-mode t))
+
+(use-package paredit
+  :ensure t
+  :init
+  (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode +1)))
+  (add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode +1)))
+  (add-hook 'lisp-mode-hook (lambda () (paredit-mode +1))))
+
+(use-package rcirc
+ :config
+ (progn
+    ;; Quit/part messages
+    (setq rcirc-id-string "")
+    (rcirc-track-minor-mode)
+    (use-package rcirc-color :ensure t)
+    (add-hook 'rcirc-mode-hook
+          (lambda ()
+                (setq rcirc-omit-mode nil)
+                (rcirc-omit-mode)
+                (set (make-local-variable 'scroll-conservatively) 8192)))
+    (defun-rcirc-command q (arg)
+    "/q alias for /query"
+    (rcirc-cmd-query arg))
+    ;; (setq rcirc-authinfo '(("freenode" nickserv "zrr" "<PASSWORD>")))
+    (setq rcirc-default-nick "zrr")
+    (setq rcirc-default-user-name "zrr")
+    (setq rcirc-default-full-name "zrr")
+    (setq rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY"))
+    (setq rcirc-server-alist
+        '(("irc.freenode.net"
+            :channels ("#emacs"))))
+    (global-set-key (kbd "C-c I") 'rcirc)))
+
+(set-register ?i '(file . "~/.emacs.d/init.el"))
+(set-register ?o '(file . "~/.emacs.d/config.org"))
+
+(column-number-mode)
+
+;; Navigate split windows w/ cursor keys
+(windmove-default-keybindings)
+
+(use-package cyberpunk-theme
+ :ensure t
+ :init 
+ (load-theme 'cyberpunk t))
 
 (setq gc-cons-threshold 800000)
-
