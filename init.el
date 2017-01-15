45c66a29e7add156c0440ebfa1444949590223bd
Zachary Rump <zrrm74@mail.missouri.edu>
2017-01-15 15:15:46 -0600

    Fix identation.

--- init.el
+++ init.el
@@ -4,10 +4,10 @@
 
 ;; Disable GUI menus
 (when window-system
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(tooltip-mode -1))
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (tooltip-mode -1))
 
 ;; Disable CJK coding/encoding 
 (setq utf-translate-cjk-mode nil)
